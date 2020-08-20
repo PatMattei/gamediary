1543498e2f32896c812fe63834fa2e1634bce18f
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-20 10:50:31 -0400

    controllers file created

---
+++ controllers/entries.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const Entry = require('../models/entries.js');
+
+// add routes
+// Index
+router.get('/', (req, res) => {
+    Entry.find({}, (error, allEntries) => {
+        res.render('entries/Index', {
+            entries: allEntries
+        })
+    });
+
+});
+
+// export router
+module.exports = router;
--- server.js
+++ server.js
@@ -11,16 +11,23 @@ app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(methodOverride('_method'));
-const mongoURI = process.env.MONGO_URI; //M
+const mongoURI = process.env.MONGO_URI;
 // mongoose connection
 
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true
-}); //M
+});
+
+//Controller
+const entriesController = require('./controllers/fruits.js');
+app.use('/entries', entriesController);
 
 // listen
 app.listen(PORT, () => {
     console.log('listening on: ' + PORT);
 });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+})

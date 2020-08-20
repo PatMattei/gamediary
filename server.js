a40a0336ad85d9a343726b76684350e3262e533b
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-20 10:38:23 -0400

    Added middleware and dependencies

--- server.js
+++ server.js
@@ -2,7 +2,15 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
+// middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
 const mongoURI = process.env.MONGO_URI; //M
 // mongoose connection
 
@@ -11,3 +19,8 @@ const PORT = process.env.PORT || 3000;
 mongoose.connect(mongoURI, {
     useNewUrlParser: true, useUnifiedTopology:true
 }); //M
+
+// listen
+app.listen(PORT, () => {
+    console.log('listening on: ' + PORT);
+});

5baf15c8fe9d8ee9e44c3659debf4fafcf95a888
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-22 15:42:57 -0400

    added head tags to Layout

--- views/components/Layout.jsx
+++ views/components/Layout.jsx
@@ -5,13 +5,18 @@ class Layout extends React.Component {
         return(
             <html>
                 <head>
+                    <meta charset="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+
                     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
                     <link rel="stylesheet" href="/resources/demos/style.css" />
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
                     {/*<link rel="stylesheet" href="/css/app.css"/>*/}
-                    <title>{this.props.title}</title>
+                    
                     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
                     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+                    <title>{this.props.title}</title>
 
                 </head>
                 <body>

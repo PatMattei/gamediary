9a381d8364948a39e42c06a7987ac67a0126689f
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-23 23:12:14 -0400

    Added basic Bootstrap styling to index page

--- views/Index.jsx
+++ views/Index.jsx
@@ -10,20 +10,21 @@ class Index extends React.Component {
                 <ul className="index-list">
                     {entries.map((entry) => {
                         return(
-                            <li className="diary-entry" key={entry._id}>
-                                <a href={`/entries/${entry._id}`}>
-                                    <p>Entry for <span className="entry-date">{entry.entryDate.toString()}</span></p>
-                                    <h2>Games played:</h2>
-                                </a>
-                                {entry.games.map((game) => {
-                                    return(
-                                        <div>
-                                            <h3 key={`${entry._id}-${game.gameName}`}>{game.gameName}</h3>
-                                            <img src={game.gameImgSrc} />
-                                        </div>
-                                    )
-                                })}
-                                <hr></hr>
+                            <li className="diary-entry d-flex w-100" key={entry._id}>
+                                <div className="entry-game-img-thumbnail-list d-flex flex-wrap col-8">
+                                    {entry.games.map((game) => {
+                                        return(
+                                            <div className="game-img-thumbnail-container col-3">
+                                                <img src={game.gameImgSrc} className="game-img-thumbnail" />
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <div className="day-diary col-2">{entry.dayEntry}</div>
+                                <div className="date-button-container col-2">
+                                    <div className="entry-date">{entry.entryDate.toString()}</div>
+                                    <a href={`/entries/${entry._id}`}><button type="button" className="view-full-entry btn btn-primary">View Full Entry</button></a>
+                                </div>
                             </li>
                         );
                     })}

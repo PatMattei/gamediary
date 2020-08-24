756a8263cef201809767c3064a4c4ba924e9283d
Patrick Mattei <patrick.mattei@gmail.com>
2020-08-24 18:44:59 -0400

    Added README file. Added Flatly theme and Bootswatch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # gamediary
+
+GameDiary can be found at https://gamediary.herokuapp.com/. It's a site where users can log their daily video-game playing habits, including specigic games they played and text entries about what they did. A user can log a general entry for the day in addition to specific entries on each game that they played.
+
+Tech Used:
+Styling is mostly done with Bootstrap and I challenge myself to stay as close to the framework as possible, without having to force my own spacing and sizing. I also used Bootswatch's Flatly theme for color and general styling.
+
+Stretch Goals:
+I’d liked to hook this up to an API to pull game entries and art
+The ability to embed media for a game entry (ex. a Twitch saved video or a screenshot the user took in the game that day)
+Actual user profiles
+Showing only a selected month’s entries on Index
+Tags for entries
--- views/components/Layout.jsx
+++ views/components/Layout.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 
+
 class Layout extends React.Component {
     render() {
         return(
@@ -9,8 +10,9 @@ class Layout extends React.Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
 
                     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
-                    <link rel="stylesheet" href="/css/app.css" />
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css" integrity="sha384-qF/QmIAj5ZaYFAeQcrQ6bfVMAh4zZlrGwTPY7T/M+iTTLJqJBJjwwnsE5Y0mV7QK" crossorigin="anonymous"></link>
                     <link rel="stylesheet" href="/css/app.css"/>
                     
                     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossOrigin="anonymous"></script>
